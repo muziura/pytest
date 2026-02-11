621ede236934bef1d055e63a755ba6d3d50772c9
muziura <iuri440@telaveli.ge>
2026-02-11 10:26:49 +0400

    გავაკეთე ფანჯარა

---
+++ clock.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title(" ჩემი პირველი პროგრამის სათაური ")
+
+root.geometry("800x600")
+
+label = tk.Label(root, text="ვინ ხარ?",font=("arial",34))
+
+label.pack()
+
+root. mainloop()
